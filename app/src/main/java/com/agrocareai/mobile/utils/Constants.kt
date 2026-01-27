f5ee717fd05d9ba756629ab671c725b782fba220
vedant-9905 <vedant.mahamunkar9905@gmail.com>
2026-01-28 00:10:50 +0530

    feat: setup MVVM folder architecture

---
+++ app/src/main/java/com/agrocareai/mobile/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.agrocareai.mobile.utils
+
+object Constants {
+    const val MODEL_PATH = "model.tflite"
+    const val LABELS_PATH = "labels.txt"
+    const val DATABASE_NAME = "agrocare_db"
+}
